aa14a3834666bee69a42af34ffe903f5c821a2cd
KindredLamar <wapgameslt@gmail.com>
2025-04-21 08:32:24 +0300

    updated top players cron

--- cronjobs/topPlayers.php
+++ cronjobs/topPlayers.php
@@ -4,12 +4,26 @@ include_once '../cfg/functions.php';
 
 logInfo('Cron job started in file: ' . basename(__FILE__));
 
-$topUsers = mysqli_query($conn,"SELECT * from zaidejai WHERE statusas != 'Kurejas' AND lygis > 1 ORDER BY lygis DESC LIMIT 3");
-$message = '**Top Players**:' . "\n";
-$nr = 1;
+$nust = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM nustatymai"));
 
-while ($row = mysqli_fetch_assoc($topUsers)) {
-    $message .= $nr++ . '. **' . $row['nick'] . '** (' . $row['lygis'] . ')' . "\n";
+if (random_int(1, 3) === 3) {
+    $topUsers = mysqli_query($conn, "SELECT * from zaidejai WHERE statusas != 'Kurejas' AND lygis > 1 ORDER BY lygis DESC LIMIT 3");
+    $message = '**Top Players By Level**:' . "\n";
+    $nr = 1;
+
+    while ($row = mysqli_fetch_assoc($topUsers)) {
+        $message .= $nr++ . '. **' . $row['nick'] . '** (' . $row['lygis'] . ')' . "\n";
+    }
+
+    sendDiscordMessage($message);
+}
+
+
+$topPlayersByActions = mysqli_query($conn, "SELECT * FROM dtop WHERE nick != '" . mysqli_real_escape_string($conn, $nust['last']) . "' ORDER BY vksm DESC LIMIT 5");
+$message = '**Top Players By Action Today**:' . "\n";
+$nr = 1;
+while ($row = mysqli_fetch_assoc($topPlayersByActions)) {
+    $message .= $nr++ . '. **' . $row['nick'] . '** (' . sk($row['vksm']) . ')' . "\n";
 }
 
 sendDiscordMessage($message);
